71f3cf408b2e19aecb75837e899328d195eb0a90
张建军 <co_zjw@163.com>
2019-06-18 18:09:40 +0800

    [add] :sparkles: support json response.

--- src/api/helloworld.py
+++ src/api/helloworld.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource
+from flask import jsonify
 
 
 class HelloWorld(Resource):
   def get(self):
-    return "HelloWorld"
+    return jsonify({"status": "success", "data": [1, 2, 3, 4]})
